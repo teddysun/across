b4205ef4e533649746b7106fe35f9c09ffba7426
Teddysun <i@teddysun.com>
2015-12-25 20:16:48 +0800

    Fix a bug

--- bench.sh
+++ bench.sh
@@ -7,7 +7,7 @@
 #==============================================================#
 
 get_opsy() {
-    [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' && return
+    [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
     [ -f /etc/lsb-release ] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
     [ -f /etc/redhat-release ] && awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release && return
 }
