79e1f654931e6d3316f66ba565bff67c92be312d
Teddysun <i@teddysun.com>
2016-02-01 21:10:41 +0900

    Fix a IO test bug in Chinese environment

--- bench.sh
+++ bench.sh
@@ -76,7 +76,7 @@ speed_v6() {
 }
 
 io_test() {
-    (dd if=/dev/zero of=test_$$ bs=64k count=16k conv=fdatasync && rm -f test_$$ ) 2>&1 | awk -F, '{io=$NF} END { print io}' | sed 's/^[ \t]*//;s/[ \t]*$//'
+    (LANG=en_US dd if=/dev/zero of=test_$$ bs=64k count=16k conv=fdatasync && rm -f test_$$ ) 2>&1 | awk -F, '{io=$NF} END { print io}' | sed 's/^[ \t]*//;s/[ \t]*$//'
 }
 
 cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//' )
