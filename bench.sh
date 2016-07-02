c7f787fb38a3c920f521c6f9d88bcedf22a72a22
Teddysun <i@teddysun.com>
2016-07-02 16:47:57 +0900

    update

--- bench.sh
+++ bench.sh
@@ -120,8 +120,11 @@ else
 fi
 
 io1=$( io_test )
+echo "I/O speed(1st run) : $io1"
 io2=$( io_test )
+echo "I/O speed(2nd run) : $io2"
 io3=$( io_test )
+echo "I/O speed(3rd run) : $io3"
 ioraw1=$( echo $io1 | awk 'NR==1 {print $1}' )
 [ "`echo $io1 | awk 'NR==1 {print $2}'`" == "GB/s" ] && ioraw1=$( awk 'BEGIN{print '$ioraw1' * 1024}' )
 ioraw2=$( echo $io2 | awk 'NR==1 {print $1}' )
@@ -130,8 +133,5 @@ ioraw3=$( echo $io3 | awk 'NR==1 {print $1}' )
 [ "`echo $io3 | awk 'NR==1 {print $2}'`" == "GB/s" ] && ioraw3=$( awk 'BEGIN{print '$ioraw3' * 1024}' )
 ioall=$( awk 'BEGIN{print '$ioraw1' + '$ioraw2' + '$ioraw3'}' )
 ioavg=$( awk 'BEGIN{print '$ioall'/3}' )
-echo "I/O speed(1st run) : $io1"
-echo "I/O speed(2nd run) : $io2"
-echo "I/O speed(3rd run) : $io3"
-echo "Average I/O: $ioavg MB/s"
+echo "Average I/O speed  : $ioavg MB/s"
 echo
