2a2a1d670d12d1eb6621d155aaed440fb66b7403
Teddysun <i@teddysun.com>
2016-07-02 12:32:36 +0900

    update

--- bench.sh
+++ bench.sh
@@ -7,9 +7,9 @@
 #==============================================================#
 
 get_opsy() {
+    [ -f /etc/redhat-release ] && awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release && return
     [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
     [ -f /etc/lsb-release ] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
-    [ -f /etc/redhat-release ] && awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release && return
 }
 
 next() {
