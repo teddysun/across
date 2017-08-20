c01dc185ca2d17a1a0cd4a60eae100398901dc0c
ddv12138 <644077730@qq.com>
2017-08-20 16:56:33 +0800

    Add files via upload

---
+++ newlinux.sh
@@ -0,0 +1,4 @@
+sudo apt install screenfetch mycli speedometer shadowsocks
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer oracle-java8-set-default
