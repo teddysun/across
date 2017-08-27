1e7f97cfe7464df585af283e7a5457c04c59e50a
ddv12138 <644077730@qq.com>
2017-08-27 13:37:58 +0800

    Sun Aug 27 13:37:58 CST 2017

--- cet_query.py
+++ cet_query.py
@@ -74,7 +74,7 @@ def append_file(string):
 	with open('cet.txt','a+') as f:
 		f.writelines(string + os.linesep)
 
-number = 420550171103519,420550171103699 ### 420550171103524 贺深
+number = 420550171103515,420550171103600 ### 420550171103524 贺深
 
 
 ### testing
@@ -85,7 +85,7 @@ else:
 	print('没有')
 exit(0)'''
 ### testing end
-names = ['赵昊罡']
+names = ['贺深','赵昊罡']
 for num in range(1,11):
     for zkzh in [i for i in range(*number)]:
         for xm in names:
