4bb6be0ab8ede1b340fb2d180c6c152d632c0d44
Teddysun <i@teddysun.com>
2020-08-31 21:35:10 +0900

    Fixed issue #72

--- wireguard.sh
+++ wireguard.sh
@@ -299,11 +299,8 @@ install_wg_1() {
     _info "Install wireguard from repository"
     case "$(_os)" in
         ubuntu)
-            _error_detect "add-apt-repository ppa:wireguard/wireguard"
             _error_detect "apt-get update"
-            _error_detect "apt-get -y install linux-headers-$(uname -r)"
-            _error_detect "apt-get -y install wireguard-dkms"
-            _error_detect "apt-get -y install wireguard-tools"
+            _error_detect "apt-get -y install wireguard"
             ;;
         debian)
             echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
