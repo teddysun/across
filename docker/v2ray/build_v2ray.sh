6b0448de1d6072b35e1986f44cb8ff86cbb1f842
Teddysun <i@teddysun.com>
2020-09-12 20:32:15 +0900

    Update build_v2ray.sh

--- docker/v2ray/build_v2ray.sh
+++ docker/v2ray/build_v2ray.sh
@@ -19,15 +19,8 @@ for CMD in "${COMMANDS[@]}"; do
 done
 
 cd ${cur_dir}
-git clone https://github.com/v2ray/v2ray-core.git
+git clone https://github.com/v2fly/v2ray-core.git
 cd v2ray-core || exit 2
-
-# Fixed issues below:
-# ../go/pkg/mod/golang.org/x/crypto@v0.0.0-20190308221718-c2843e01d9a2/poly1305/sum_s390x.go:29:5: undefined: cpu.S390X
-# ../go/pkg/mod/golang.org/x/crypto@v0.0.0-20190308221718-c2843e01d9a2/sha3/sha3_s390x.go:235:5: undefined: cpu.S390X
-
-sed -i "s@golang.org/x/crypto.*@golang.org/x/crypto master@" go.mod
-go mod tidy
 
 LDFLAGS="-s -w"
 ARCHS=( 386 amd64 arm arm64 ppc64le s390x )
@@ -38,12 +31,12 @@ for ARCH in ${ARCHS[@]}; do
         for V in ${ARMS[@]}; do
             echo "Building v2ray_linux_${ARCH}${V} and v2ctl_linux_${ARCH}${V}"
             env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GOARM=${V} go build -v -ldflags "${LDFLAGS}" -o ${cur_dir}/v2ray_linux_${ARCH}${V} ./main
-            env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GOARM=${V} go build -v -ldflags "${LDFLAGS}" -o ${cur_dir}/v2ctl_linux_${ARCH}${V} ./infra/control/main
+            env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GOARM=${V} go build -v -ldflags "${LDFLAGS}" -tags confonly -o ${cur_dir}/v2ctl_linux_${ARCH}${V} ./infra/control/main
         done
     else
         echo "Building v2ray_linux_${ARCH} and v2ctl_linux_${ARCH}"
         env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build -v -ldflags "${LDFLAGS}" -o ${cur_dir}/v2ray_linux_${ARCH} ./main
-        env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build -v -ldflags "${LDFLAGS}" -o ${cur_dir}/v2ctl_linux_${ARCH} ./infra/control/main
+        env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build -v -ldflags "${LDFLAGS}" -tags confonly -o ${cur_dir}/v2ctl_linux_${ARCH} ./infra/control/main
     fi
 done
 
