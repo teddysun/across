26e0fe3f36cca572b5419900d7b6fd8b249b9891
Teddysun <i@teddysun.com>
2021-02-11 15:09:56 +0900

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -53,7 +53,7 @@ _exit() {
 }
 
 get_opsy() {
-    [ -f /etc/redhat-release ] && awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release && return
+    [ -f /etc/redhat-release ] && awk '{print $0}' /etc/redhat-release && return
     [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
     [ -f /etc/lsb-release ] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
 }
