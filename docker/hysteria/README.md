e1acb6130131442c4b0e996f76259ac1ddcab363
Teddysun <i@teddysun.com>
2022-06-22 20:30:28 +0900

    Added hysteria Docker Image

---
+++ docker/hysteria/README.md
@@ -0,0 +1,53 @@
+## Hysteria Docker Image by Teddysun
+
+[Hysteria][1] is a feature-packed proxy & relay utility optimized for lossy, unstable connections, powered by a customized QUIC protocol.
+
+Docker images are built for quick deployment in various computing cloud providers.
+
+For more information on docker and containerization technologies, refer to [official document][2].
+
+## Prepare the host
+
+If you need to install docker by yourself, follow the [official installation guide][3].
+
+## Pull the image
+
+```bash
+$ docker pull teddysun/hysteria
+```
+
+This pulls the latest release of Hysteria.
+
+It can be found at [Docker Hub][4].
+
+## Start a container
+
+You **must create a configuration file**  `/etc/hysteria/server.json` in host at first:
+
+```
+$ mkdir -p /etc/hysteria
+```
+
+A sample in JSON like below:
+
+```
+{
+    "listen": ":8998",
+    "cert": "/etc/hysteria/cert.crt",
+    "key": "/root/hysteria/private.key",
+    "obfs": "dGVkZHlzdW4uY29tCg=="
+}
+```
+
+There is an example to start a container that listen on port `8998`, run as a Hysteria server like below:
+
+```bash
+$ docker run -d -p 8998:8998 --name hysteria --restart=always -v /etc/hysteria:/etc/hysteria teddysun/hysteria
+```
+
+**Warning**: The port number must be same as configuration and opened in firewall.
+
+[1]: https://github.com/HyNetwork/hysteria
+[2]: https://docs.docker.com/
+[3]: https://docs.docker.com/install/
+[4]: https://hub.docker.com/r/teddysun/hysteria/
---
+++ docker/hysteria/build_hysteria.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+#
+# This is a Shell script for build multi-architectures hysteria binary file
+# 
+# Supported architectures: amd64, arm32v6, arm32v7, arm64v8, i386, ppc64le, s390x
+# 
+# Copyright (C) 2022 Teddysun <i@teddysun.com>
+#
+# Reference URL:
+# https://github.com/HyNetwork/hysteria
+
+cur_dir="$(pwd)"
+
+COMMANDS=( git go )
+for CMD in "${COMMANDS[@]}"; do
+    if [ ! "$(command -v "${CMD}")" ]; then
+        echo "${CMD} is not installed, please install it and try again" && exit 1
+    fi
+done
+
+cd ${cur_dir}
+echo "git clone https://github.com/HyNetwork/hysteria.git"
+git clone https://github.com/HyNetwork/hysteria.git
+cd hysteria || exit 2
+
+VERSION="$(git describe)"
+COMMIT="$(git rev-parse HEAD)"
+TIMESTAMP="$(date "+%F")"
+
+LDFLAGS="-s -w -X 'main.appVersion=${VERSION}' -X 'main.appCommit=${COMMIT}' -X 'main.appDate=${TIMESTAMP}' -buildid="
+ARCHS=( 386 amd64 arm arm64 ppc64le s390x )
+ARMS=( 6 7 )
+
+for ARCH in ${ARCHS[@]}; do
+    if [ "${ARCH}" = "arm" ]; then
+        for V in ${ARMS[@]}; do
+            echo "Building hysteria_linux_${ARCH}${V}"
+            env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GOARM=${V} go build -v -trimpath -ldflags "${LDFLAGS}" -o ${cur_dir}/hysteria_linux_${ARCH}${V} ./cmd || exit 1
+        done
+    else
+        echo "Building hysteria_linux_${ARCH}"
+        env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build -v -trimpath -ldflags "${LDFLAGS}" -o ${cur_dir}/hysteria_linux_${ARCH} ./cmd || exit 1
+    fi
+done
+
+ARCHS=( 386 amd64 )
+for ARCH in ${ARCHS[@]}; do
+    echo "Building hysteria_windows_${ARCH}.exe"
+    env CGO_ENABLED=0 GOOS=windows GOARCH=${ARCH} go build -v -trimpath -ldflags "${LDFLAGS}" -o ${cur_dir}/hysteria_windows_${ARCH}.exe ./cmd
+done
+
+chmod +x ${cur_dir}/hysteria_*
+# clean up
+cd ${cur_dir} && rm -fr hysteria
