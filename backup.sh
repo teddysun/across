9bce48eae5657388731ce6f88918d976245f5da1
Teddysun <i@teddysun.com>
2026-02-04 10:10:32 +0900

    Update comments refactor(backup.sh): improve openssl encryption security and code quality
    Security Improvements:
    - Update openssl enc from -aes256 to -aes-256-cbc with PBKDF2
    - Add -pbkdf2 flag for modern key derivation (replaces EVP_BytesToKey)
    - Add -iter 100000 for 100,000 PBKDF2 iterations
    - Change -md from sha1 to sha256 for better security
    - Update decryption command documentation accordingly
    
    Code Quality Improvements:
    - Use [[ ... ]] instead of [ ... ] for conditionals
    - Add proper quoting to all variable references
    - Use +=() for array append operations
    - Separate local declaration from assignment to avoid masking return values
    
    Signed-off-by: Teddysun <i@teddysun.com>

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (C) 2013 - 2022 Teddysun <i@teddysun.com>
+# Copyright (C) 2013 - 2026 Teddysun <i@teddysun.com>
 # 
 # This file is part of the LAMP script.
 #
@@ -32,7 +32,7 @@ ENCRYPTFLG=true
 # WARNING: KEEP THE PASSWORD SAFE!!!
 # The password used to encrypt the backup
 # To decrypt backups made by this script, run the following command:
-# openssl enc -aes256 -in [encrypted backup] -out decrypted_backup.tgz -pass pass:[backup password] -d -md sha1
+# openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -md sha256 -in [encrypted backup] -out decrypted_backup.tgz -pass pass:[backup password] -d
 BACKUPPASS="mypassword"
 
 # Directory to store backups
@@ -117,9 +117,9 @@ check_commands() {
     # This section checks for all of the binaries used in the backup
     # Do not check mysql command if you do not want to backup the MySQL database
     if [ -z "${MYSQL_ROOT_PASSWORD}" ]; then
-        BINARIES=( cat cd du date dirname echo openssl pwd rm tar )
+        BINARIES=(cat cd du date dirname echo openssl pwd rm tar)
     else
-        BINARIES=( cat cd du date dirname echo openssl mysql mysqldump pwd rm tar )
+        BINARIES=(cat cd du date dirname echo openssl mysql mysqldump pwd rm tar)
     fi
 
     # Iterate over the list of binaries, and if one isn't found, abort
@@ -146,9 +146,10 @@ check_commands() {
 }
 
 calculate_size() {
-    local file_name=$1
-    local file_size=$(du -h $file_name 2>/dev/null | awk '{print $1}')
-    if [ "x${file_size}" = "x" ]; then
+    local file_name="$1"
+    local file_size
+    file_size=$(du -h "${file_name}" 2>/dev/null | awk '{print $1}')
+    if [[ -z "${file_size}" ]]; then
         echo "unknown"
     else
         echo "${file_size}"
@@ -164,31 +165,34 @@ mysql_backup() {
         mysql -u root -p"${MYSQL_ROOT_PASSWORD}" 2>/dev/null <<EOF
 exit
 EOF
-        if [ $? -ne 0 ]; then
+        # shellcheck disable=SC2181
+        if [[ $? -ne 0 ]]; then
             log "MySQL root password is incorrect. Please check it and try again"
             exit 1
         fi
-        if [[ "${MYSQL_DATABASE_NAME[@]}" == "" ]]; then
+        if [[ -z "${MYSQL_DATABASE_NAME[*]}" ]]; then
             mysqldump -u root -p"${MYSQL_ROOT_PASSWORD}" --all-databases > "${SQLFILE}" 2>/dev/null
-            if [ $? -ne 0 ]; then
+            # shellcheck disable=SC2181
+            if [[ $? -ne 0 ]]; then
                 log "MySQL all databases backup failed"
                 exit 1
             fi
             log "MySQL all databases dump file name: ${SQLFILE}"
             #Add MySQL backup dump file to BACKUP list
-            BACKUP=(${BACKUP[@]} ${SQLFILE})
+            BACKUP=("${BACKUP[@]}" "${SQLFILE}")
         else
-            for db in ${MYSQL_DATABASE_NAME[@]}; do
+            for db in "${MYSQL_DATABASE_NAME[@]}"; do
                 unset DBFILE
                 DBFILE="${TEMPDIR}${db}_${BACKUPDATE}.sql"
-                mysqldump -u root -p"${MYSQL_ROOT_PASSWORD}" ${db} > "${DBFILE}" 2>/dev/null
-                if [ $? -ne 0 ]; then
+                mysqldump -u root -p"${MYSQL_ROOT_PASSWORD}" "${db}" > "${DBFILE}" 2>/dev/null
+                # shellcheck disable=SC2181
+                if [[ $? -ne 0 ]]; then
                     log "MySQL database name [${db}] backup failed, please check database name is correct and try again"
                     exit 1
                 fi
                 log "MySQL database name [${db}] dump file name: ${DBFILE}"
                 #Add MySQL backup dump file to BACKUP list
-                BACKUP=(${BACKUP[@]} ${DBFILE})
+                BACKUP=("${BACKUP[@]}" "${DBFILE}")
             done
         fi
         log "MySQL dump completed"
@@ -196,11 +200,11 @@ EOF
 }
 
 start_backup() {
-    [ "${#BACKUP[@]}" -eq 0 ] && echo "Error: You must to modify the [$(basename $0)] config before run it!" && exit 1
+    [[ ${#BACKUP[@]} -eq 0 ]] && echo "Error: You must to modify the [$(basename "$0")] config before run it!" && exit 1
 
     log "Tar backup file start"
-    tar -zcPf ${TARFILE} ${BACKUP[@]}
-    if [ $? -gt 1 ]; then
+    tar -zcPf "${TARFILE}" "${BACKUP[@]}"
+    if [[ $? -gt 1 ]]; then
         log "Tar backup file failed"
         exit 1
     fi
@@ -209,18 +213,18 @@ start_backup() {
     # Encrypt tar file
     if ${ENCRYPTFLG}; then
         log "Encrypt backup file start"
-        openssl enc -aes256 -in "${TARFILE}" -out "${ENC_TARFILE}" -pass pass:"${BACKUPPASS}" -md sha1
+        openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -md sha256 -in "${TARFILE}" -out "${ENC_TARFILE}" -pass pass:"${BACKUPPASS}"
         log "Encrypt backup file completed"
 
         # Delete unencrypted tar
         log "Delete unencrypted tar file: ${TARFILE}"
-        rm -f ${TARFILE}
+        rm -f "${TARFILE}"
     fi
 
     # Delete MySQL temporary dump file
-    for sql in $(ls ${TEMPDIR}*.sql 2> /dev/null); do
+    for sql in "${TEMPDIR}"*.sql; do
         log "Delete MySQL temporary dump file: ${sql}"
-        rm -f ${sql}
+        rm -f "${sql}"
     done
 
     if ${ENCRYPTFLG}; then
@@ -228,7 +232,7 @@ start_backup() {
     else
         OUT_FILE="${TARFILE}"
     fi
-    log "File name: ${OUT_FILE}, File size: $(calculate_size ${OUT_FILE})"
+    log "File name: ${OUT_FILE}, File size: $(calculate_size "${OUT_FILE}")"
 }
 
 # Transfer backup file to Google Drive
@@ -236,17 +240,15 @@ start_backup() {
 # https://rclone.org/downloads/
 rclone_upload() {
     if ${RCLONE_FLG} && ${RCLONE_COMMAND}; then
-        [ -z "${RCLONE_NAME}" ] && log "Error: RCLONE_NAME can not be empty!" && return 1
-        if [ -n "${RCLONE_FOLDER}" ]; then
-            rclone ls ${RCLONE_NAME}:${RCLONE_FOLDER} 2>&1 > /dev/null
-            if [ $? -ne 0 ]; then
+        [[ -z "${RCLONE_NAME}" ]] && log "Error: RCLONE_NAME can not be empty!" && return 1
+        if [[ -n "${RCLONE_FOLDER}" ]]; then
+            if ! rclone ls "${RCLONE_NAME}:${RCLONE_FOLDER}" > /dev/null 2>&1; then
                 log "Create the path ${RCLONE_NAME}:${RCLONE_FOLDER}"
-                rclone mkdir ${RCLONE_NAME}:${RCLONE_FOLDER}
+                rclone mkdir "${RCLONE_NAME}:${RCLONE_FOLDER}"
             fi
         fi
         log "Tranferring backup file: ${OUT_FILE} to Google Drive"
-        rclone copy ${OUT_FILE} ${RCLONE_NAME}:${RCLONE_FOLDER} >> ${LOGFILE}
-        if [ $? -ne 0 ]; then
+        if ! rclone copy "${OUT_FILE}" "${RCLONE_NAME}:${RCLONE_FOLDER}" >> "${LOGFILE}"; then
             log "Error: Tranferring backup file: ${OUT_FILE} to Google Drive failed"
             return 1
         fi
@@ -257,21 +259,23 @@ rclone_upload() {
 # Tranferring backup file to FTP server
 ftp_upload() {
     if ${FTP_FLG}; then
-        [ -z "${FTP_HOST}" ] && log "Error: FTP_HOST can not be empty!" && return 1
-        [ -z "${FTP_USER}" ] && log "Error: FTP_USER can not be empty!" && return 1
-        [ -z "${FTP_PASS}" ] && log "Error: FTP_PASS can not be empty!" && return 1
-        [ -z "${FTP_DIR}" ] && log "Error: FTP_DIR can not be empty!" && return 1
-        local FTP_OUT_FILE=$(basename ${OUT_FILE})
+        [[ -z "${FTP_HOST}" ]] && log "Error: FTP_HOST can not be empty!" && return 1
+        [[ -z "${FTP_USER}" ]] && log "Error: FTP_USER can not be empty!" && return 1
+        [[ -z "${FTP_PASS}" ]] && log "Error: FTP_PASS can not be empty!" && return 1
+        [[ -z "${FTP_DIR}" ]] && log "Error: FTP_DIR can not be empty!" && return 1
+        local FTP_OUT_FILE
+        FTP_OUT_FILE=$(basename "${OUT_FILE}")
         log "Tranferring backup file: ${FTP_OUT_FILE} to FTP server"
-        ftp -in ${FTP_HOST} 2>&1 >> ${LOGFILE} <<EOF
-user $FTP_USER $FTP_PASS
+        ftp -in "${FTP_HOST}" 2>&1 >> "${LOGFILE}" <<EOF
+user ${FTP_USER} ${FTP_PASS}
 binary
-lcd $LOCALDIR
-cd $FTP_DIR
-put $FTP_OUT_FILE
+lcd ${LOCALDIR}
+cd ${FTP_DIR}
+put ${FTP_OUT_FILE}
 quit
 EOF
-        if [ $? -ne 0 ]; then
+        # shellcheck disable=SC2181
+        if [[ $? -ne 0 ]]; then
             log "Error: Tranferring backup file: ${FTP_OUT_FILE} to FTP server failed"
             return 1
         fi
@@ -281,14 +285,15 @@ EOF
 
 # Get file date
 get_file_date() {
-    #Approximate a 30-day month and 365-day year
+
+    # Approximate a 30-day month and 365-day year
     DAYS=$(( $((10#${YEAR}*365)) + $((10#${MONTH}*30)) + $((10#${DAY})) ))
     unset FILEYEAR FILEMONTH FILEDAY FILEDAYS FILEAGE
     FILEYEAR=$(echo "$1" | cut -d_ -f2 | cut -c 1-4)
     FILEMONTH=$(echo "$1" | cut -d_ -f2 | cut -c 5-6)
     FILEDAY=$(echo "$1" | cut -d_ -f2 | cut -c 7-8)
     if [[ "${FILEYEAR}" && "${FILEMONTH}" && "${FILEDAY}" ]]; then
-        #Approximate a 30-day month and 365-day year
+        # Approximate a 30-day month and 365-day year
         FILEDAYS=$(( $((10#${FILEYEAR}*365)) + $((10#${FILEMONTH}*30)) + $((10#${FILEDAY})) ))
         FILEAGE=$(( 10#${DAYS} - 10#${FILEDAYS} ))
         return 0
@@ -298,12 +303,10 @@ get_file_date() {
 
 # Delete Google Drive's old backup file
 delete_gdrive_file() {
-    local FILENAME=$1
+    local FILENAME="$1"
     if ${DELETE_REMOTE_FILE_FLG} && ${RCLONE_COMMAND}; then
-        rclone ls ${RCLONE_NAME}:${RCLONE_FOLDER}/${FILENAME} 2>&1 > /dev/null
-        if [ $? -eq 0 ]; then
-            rclone delete ${RCLONE_NAME}:${RCLONE_FOLDER}/${FILENAME} >> ${LOGFILE}
-            if [ $? -eq 0 ]; then
+        if rclone ls "${RCLONE_NAME}:${RCLONE_FOLDER}/${FILENAME}" > /dev/null 2>&1; then
+            if rclone delete "${RCLONE_NAME}:${RCLONE_FOLDER}/${FILENAME}" >> "${LOGFILE}"; then
                 log "Google Drive's old backup file: ${FILENAME} has been deleted"
             else
                 log "Failed to delete Google Drive's old backup file: ${FILENAME}"
@@ -316,15 +319,16 @@ delete_gdrive_file() {
 
 # Delete FTP server's old backup file
 delete_ftp_file() {
-    local FILENAME=$1
+    local FILENAME="$1"
     if ${DELETE_REMOTE_FILE_FLG} && ${FTP_FLG}; then
-        ftp -in ${FTP_HOST} 2>&1 >> ${LOGFILE} <<EOF
-user $FTP_USER $FTP_PASS
-cd $FTP_DIR
-del $FILENAME
+        ftp -in "${FTP_HOST}" 2>&1 >> "${LOGFILE}" <<EOF
+user ${FTP_USER} ${FTP_PASS}
+cd ${FTP_DIR}
+del ${FILENAME}
 quit
 EOF
-        if [ $? -eq 0 ]; then
+        # shellcheck disable=SC2181
+        if [[ $? -eq 0 ]]; then
             log "FTP server's old backup file: ${FILENAME} has been deleted"
         else
             log "Failed to delete FTP server's old backup file: ${FILENAME}"
@@ -334,20 +338,21 @@ EOF
 
 # Clean up old file
 clean_up_files() {
-    cd ${LOCALDIR} || exit
+    cd "${LOCALDIR}" || exit
     if ${ENCRYPTFLG}; then
-        LS=($(ls *.enc 2> /dev/null))
+        LS=(*.enc)
+        [[ -e "${LS[0]}" ]] || LS=()
     else
-        LS=($(ls *.tgz 2> /dev/null))
+        LS=(*.tgz)
+        [[ -e "${LS[0]}" ]] || LS=()
     fi
-    for f in ${LS[@]}; do
-        get_file_date ${f}
-        if [ $? -eq 0 ]; then
+    for f in "${LS[@]}"; do
+        if get_file_date "${f}"; then
             if [[ ${FILEAGE} -gt ${LOCALAGEDAILIES} ]]; then
-                rm -f ${f}
+                rm -f "${f}"
                 log "Old backup file name: ${f} has been deleted"
-                delete_gdrive_file ${f}
-                delete_ftp_file ${f}
+                delete_gdrive_file "${f}"
+                delete_ftp_file "${f}"
             fi
         fi
     done
@@ -357,8 +362,8 @@ clean_up_files() {
 STARTTIME=$(date +%s)
 
 # Check if the backup folders exist and are writeable
-[ ! -d "${LOCALDIR}" ] && mkdir -p ${LOCALDIR}
-[ ! -d "${TEMPDIR}" ] && mkdir -p ${TEMPDIR}
+[[ ! -d "${LOCALDIR}" ]] && mkdir -p "${LOCALDIR}"
+[[ ! -d "${TEMPDIR}" ]] && mkdir -p "${TEMPDIR}"
 
 log "Backup progress start"
 check_commands
